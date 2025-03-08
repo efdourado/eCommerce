dd2fd37bf2e9c7422abfe1bfbe16a14289ed6e17
efdourado <ed320819@gmail.com>
2025-03-08 01:13:47 -0300

    Code cleanup and minor fixes

--- db/e-commerce.sql
+++ db/e-commerce.sql
@@ -1,97 +1,81 @@
-create table "Supplier" (
-	"id" blob,
-	"name" varchar(255) not null,
-	primary key("id")
+create table "supplier" (
+	"id" int primary key,
+	"name" varchar(255) not null
 );
 
-create table "Category" (
-	"id" blob,
-	"name" varchar(255) not null,
-	primary key("id")
+create table "category" (
+	"id" int primary key,
+	"name" varchar(255) not null
 );
 
-create table "Customer" (
-	"id" blob,
+create table "user" (
+	"id" int primary key,
 	"name" varchar(255) not null,
-	"customer_type" blob not null,
 	"doc" varchar(255) not null unique,
-	"email" varchar(255) unique,
-	primary key("id")
+	"email" varchar(255) not null unique,
+	"password" varchar(255) not null,
+	"phone" varchar(255)
 );
 
-create table "Seller" (
-	"id" blob,
-	"name" varchar(255) not null,
-	"seller_type" blob not null,
-	"doc" varchar(255) not null unique,
-	"email" varchar(255) unique,
-	"phone" varchar(255),
-	primary key("id")
-);
-
-create table "Product" (
-	"id" blob,
+create table "product" (
+	"id" int primary key,
 	"name" varchar(255) not null,
 	"supplier_id" int not null,
 	"category_id" int not null,
-	"seller_id" int not null,
-	primary key("id")
+	"price" float,
+	foreign key("supplier_id") references "supplier"("id") on update cascade on delete set null,
+	foreign key("category_id") references "category"("id") on update cascade on delete set null
 );
 
-create table "Order" (
-	"id" blob,
-	"customer_id" int not null,
+create type "status_t" as enum ('delivered', 'canceled', 'payment_confirmed', 'payment_failed', 'pending');
+
+create table "order" (
+	"id" int primary key,
+	"user_id" int not null,
 	"order_date" date not null,
-	"delivery_date" date,
-	"status" blob default Pending,
-	"payment_method" blob not null,
-	"city" varchar(255) not null,
-	"street" varchar(255) not null,
-	"CEP" varchar(20) not null,
-	primary key("id")
+	"status" status_t not null default 'pending',
+	"total" float not null,
+	foreign key("user_id") references "user"("id") on update cascade on delete cascade
 );
 
-create table "OrderProduct" (
+create table "order_product" (
 	"product_id" int not null,
 	"order_id" int not null,
 	"quantity" int not null default 1,
-	primary key("product_id", "order_id")
+	primary key("product_id", "order_id"),
+	foreign key("product_id") references "product"("id") on update cascade on delete cascade,
+	foreign key("order_id") references "order"("id") on update cascade on delete cascade
 );
 
-create table "Stock" (
+create table "stock" (
 	"supplier_id" int not null,
 	"product_id" int not null,
-	primary key("supplier_id", "product_id")
+	primary key("supplier_id", "product_id"),
+	foreign key("supplier_id") references "supplier"("id") on update cascade on delete cascade,
+	foreign key("product_id") references "product"("id") on update cascade on delete cascade
 );
 
-alter table "Product"
-add foreign key("supplier_id") REFERENCES "Supplier"("id")
-on update cascade on delete set null;
+create table "address" (
+	"id" serial not null unique,
+	"country" varchar(255) not null,
+	"city" varchar(255) not null,
+	"adress_line1" varchar(255),
+	"postal_code" varchar(255) not null,
+	primary key("id")
+);
 
-alter table "Product"
-add foreign key("category_id") REFERENCES "Category"("id")
-on update cascade on delete set null;
+create table "user_address" (
+	"id" serial not null unique,
+	"user_id" int,
+	"address_id" int,
+	primary key("id"),
+	foreign key("user_id") references "user"("id"),
+	foreign key("address_id") references "address"("id")
+);
 
-alter table "Product"
-add foreign key("seller_id") REFERENCES "Seller"("id")
-on update cascade on delete set null;
-
-alter table "Order"
-add foreign key("customer_id") REFERENCES "Customer"("id")
-on update cascade on delete cascade;
-
-alter table "OrderProduct"
-add foreign key("product_id") REFERENCES "Product"("id")
-on update cascade on delete cascade;
-
-alter table "OrderProduct"
-add foreign key("order_id") REFERENCES "Order"("id")
-on update cascade on delete cascade;
-
-alter table "Stock"
-add foreign key("supplier_id") REFERENCES "Supplier"("id")
-on update cascade on delete cascade;
-
-alter table "Stock"
-add foreign key("product_id") REFERENCES "Product"("id")
-on update cascade on delete cascade;
+create table "user_payment_method" (
+	"id" serial not null unique,
+	"user_id" int,
+	primary key("id"),
+	foreign key("user_id") references "user"("id")
+);
